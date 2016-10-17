cfd2bab63afcfdf7281c4a66189efac37020d072
root <root@debi>
2016-10-18 00:13:32 +0200

    Ficheros de ajax y menu responsive

---
+++ responsive/index3.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CSS Only Navigation Menu</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/style3.css">
+
+</head>
+<body>
+
+  	<header>
+  		<div class="menu_bar">
+  			<a href="#" class="bt-menu"><span class="icon-list2"></span>Menu</a>
+  		</div>
+
+  		<nav>
+  			<ul>
+  				<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
+  				<li><a href="#"><span class="icon-suitcase"></span>Trabajos</a></li>
+  				<li><a href="#"><span class="icon-rocket"></span>Proyectos</a></li>
+  				<li><a href="#"><span class="icon-earth"></span>Servicios</a></li>
+  				<li><a href="#"><span class="icon-mail"></span>Contactos</a></li>
+  			</ul>
+  		</nav>
+  	</header>
+
+    <section>
+    		<h3>Haz la pantalla mas pequeña para que puedas ver el menu en la version de dispositivo movil.</h3>
+    		<h4>Puedes presionar Ctrl + Shift + M en firefox para cambiar el tamaño de la pantalla</h4>
+    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    	</section>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    	<script src="js/menu3.js"></script>
+
+</body>
+</html>
